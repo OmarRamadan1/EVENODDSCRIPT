433ca8bc3d37e52b08328191b98fc50540600b3a
omar yasser <136138901+omaryasser3@users.noreply.github.com>
2023-08-23 19:46:48 +0300

    2nd change
    another way of comparing

--- ODDEVEN.sh
+++ ODDEVEN.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/bash
 echo -n "Please enter the number: "
 read num
-if [ $((num % 2)) -eq 0 ]; then
-    echo "The number is even."
+if ((num % 2 == 0)); then
+    echo "The number is even"
 else
-    echo "The number is odd."
+    echo "The number is odd"
 fi
